55405adc177c382bdd53721b226c05df124ef6aa
arnaudram <52896739+arnaudram@users.noreply.github.com>
2020-04-27 08:52:04 -0700

    step-05-navigation-listener

--- app/src/main/java/com/example/savingusersdatabase/MainActivity.kt
+++ app/src/main/java/com/example/savingusersdatabase/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
@@ -22,8 +23,15 @@ class MainActivity : AppCompatActivity() {
         val navController=this.findNavController(R.id.fragment4)
        // val appBarConfiguration= AppBarConfiguration(navController.graph,drawerLayout)
        NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout)
+        
         NavigationUI.setupWithNavController(binding.navView,navController)
-
+       navController.addOnDestinationChangedListener { controller, destination, arguments ->
+           if (destination.id==controller.graph.startDestination){
+              drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+           }
+           else
+               drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+       }
 
     }
 
